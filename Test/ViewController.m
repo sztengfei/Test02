e04521a851ff9a2dc7c55a48a66fff7e828782d0
滕飞 <tengfei@carelanddeMacBook-Pro.local>
2018-01-23 17:41:02 +0800

    输出Hello World!

--- Test/ViewController.m
+++ Test/ViewController.m
@@ -15,13 +15,9 @@
 @implementation ViewController
 
 - (void)viewDidLoad {
+    NSLog(@"hello world!");
     [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
 }
 
-- (void)didReceiveMemoryWarning {
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
-}
 
 @end
